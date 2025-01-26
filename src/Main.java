c4b8fa6fb3648d445de04695f21c3da7b2dcd393
Daniyar <donik12rrr@gmail.com>
2025-01-26 16:36:54 +0600

    Divided some parts for easier readins & added checking for duplicate guesses

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
+
         ArrayList<Integer> Cargo_coordinates = new ArrayList<Integer>();
+
         int first_cargo = rand.nextInt(7)+1;
         int second_cargo = rand.nextInt(7)+1;
         int third_cargo = rand.nextInt(7)+1;
+
         while (second_cargo == first_cargo){
             second_cargo = rand.nextInt(7)+1;
         }
         while (third_cargo == first_cargo || third_cargo == second_cargo){
             third_cargo = rand.nextInt(7)+1;
         }
+
         Cargo_coordinates.add(first_cargo);
         Cargo_coordinates.add(second_cargo);
         Cargo_coordinates.add(third_cargo);
+
         ArrayList <Integer> guesses = Get_guesses(sc);
+        if (guesses.get(1).equals(guesses.get(0))||(guesses.get(2).equals(guesses.get(1))||(guesses.get(2).equals(guesses.get(0))))) {
+            System.out.println("You entered two duplicate numbers, try again");
+            guesses = Get_guesses(sc);
+        }
+
     }
     public static ArrayList Get_guesses(Scanner sc){
         ArrayList <Integer> Guesses = new ArrayList<Integer>();
